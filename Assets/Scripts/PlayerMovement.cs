3f5c63493be09a06bc92d0ad4ad4dbc5b9eddf0a
Domenico Rotolo <rotolonico@gmail.com>
2026-02-01 13:30:45 +0100

    Fixed collider

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed = 7f;
     [SerializeField] private Animator animator;
+    [SerializeField] private Rigidbody rb;
 
     private bool lastWalkedRight;
     
@@ -12,32 +13,29 @@ public class PlayerMovement : MonoBehaviour
     {
         if (WindowManager.windowManager.windowOpened || !GameManager.gameManager.gameStarted) return;
 
-        var isWalkingVal = 0;
-        
-        if (Input.GetAxisRaw("Horizontal") > 0)
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 movement = new Vector3(horizontal, 0f, vertical).normalized * speed;
+        rb.linearVelocity = new Vector3(movement.x, rb.linearVelocity.y, movement.z);
+
+        int isWalkingVal = 0;
+
+        if (horizontal > 0)
         {
-            transform.position += Vector3.right * (Time.deltaTime * speed);
             lastWalkedRight = true;
             isWalkingVal = 1;
         }
-        else if (Input.GetAxisRaw("Horizontal") < 0)
+        else if (horizontal < 0)
         {
-            transform.position += Vector3.left * (Time.deltaTime * speed);
             lastWalkedRight = false;
             isWalkingVal = 2;
         }
-        
-        if (Input.GetAxisRaw("Vertical") > 0)
+        else if (vertical != 0)
         {
-            transform.position += Vector3.forward * (Time.deltaTime * speed);
             isWalkingVal = lastWalkedRight ? 1 : 2;
         }
-        else if (Input.GetAxisRaw("Vertical") < 0)
-        {
-            transform.position += Vector3.back * (Time.deltaTime * speed);
-            isWalkingVal = lastWalkedRight ? 1 : 2;
-        }
-        
+
         animator.SetInteger("IsWalking", isWalkingVal);
     }
     
